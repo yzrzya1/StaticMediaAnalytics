51b908d97ae0d6a9123e73fa11b9d421db4b6c9f
DavidCube <yibo.david.dong@gmail.com>
2015-08-26 11:13:29 -0700

    change component with yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yoApp
+ * @description
+ * # yoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yoApp', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
